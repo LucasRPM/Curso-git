70af2cd2470ecc25720d4b3ea31c6d1eda8f8aaf
Lucas <myrapha_pedroso@hotmail.com>
2021-09-27 10:00:35 -0300

    Acrescentada a pergunta sobre como voce esta

--- Projeto_Teste/Program.cs
+++ Projeto_Teste/Program.cs
@@ -7,8 +7,8 @@ namespace Projeto_Teste
         static void Main(string[] args)
         {
             Console.WriteLine("Olá Mundo!");
-
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está hoje?");
         }
     }
 }
